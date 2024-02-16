a626966ae18ac7e941e634c2ad48dd196d0cb83f
MelvynKam <152378744+MelvynKam@users.noreply.github.com>
2024-02-16 16:20:30 +0100

    detection de cercle autre méthode

--- detection_cercle.py
+++ detection_cercle.py
@@ -1,46 +1,15 @@
 import cv2
 import numpy as np
-import imutils
 
-# Load an image
-img = cv2.imread('coup1.png')
-
-# Resize the image if its width is greater than 600 pixels
-if img.shape[1] > 600:
-    img = imutils.resize(img, width=600)
-
-# Convert the image to grayscale
+img = cv2.imread('3.jpg')
+output = img.copy()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray= cv2.medianBlur(gray, 5)
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=40)
+detected_circles= np.uint16(np.around(circles))
+for(x,y,r) in detected_circles[0, :] :
+    cv2.circle(output, (x, y), r, (0,255,0),3)
+    cv2.circle(output, (x, y), r, (0,255,0),3)
 
-# Threshold the grayscale image
-ret, gray_threshed = cv2.threshold(gray, 8, 255, cv2.THRESH_BINARY)
-
-# Blur the thresholded image
-bilateral_filtered_image = cv2.bilateralFilter(gray_threshed, 5, 175, 175)
-
-# Detect edges using Canny edge detector
-edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
-
-# Find contours in the edge-detected image
-contours, _ = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-contour_list = []
-for contour in contours:
-    # Approximate contours to polygons and check if it's a circle
-    approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
-    area = cv2.contourArea(contour)
-    if len(approx) > 8 and area > 10:
-        contour_list.append(contour)
-
-
-# Draw contours on the original image
-clone = img.copy()
-cv2.drawContours(clone, contour_list, -1, (255, 0, 0), 2)
-
-# Print the number of circles detected
-print('Number of found circles: {}'.format(int(len(contour_list))))
-
-# Display the result
-cv2.imshow('Objects Detected', clone)
+cv2.imshow('output', output)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
