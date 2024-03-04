09231f767b2d3cda47d26f1ae94a2fa5bf39f927
MelvynKam <152378744+MelvynKam@users.noreply.github.com>
2024-03-04 16:39:22 +0100

    détection de couleur marche

--- detection_cercle.py
+++ detection_cercle.py
@@ -11,53 +11,49 @@ def detect_circles(image_path):
     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=30, maxRadius=60)
 
     circle_count = 0  
+    circle_centers = []  
+    circle_colors = []  
 
     if circles is not None:
         detected_circles = np.uint16(np.around(circles))
-        centers_list = []  
 
         for (x, y, r) in detected_circles[0, :]:
-            
+            r-=10
             roi = binary[y-r:y+r, x-r:x+r]
             mean_val = np.mean(roi)
-
-            cv2.circle(output, (x, y), r, (0, 255, 0) if mean_val >= 68 else (255, 0, 0), 3)
-
-            centers_list.append((x, y, r))  
+            print (x,y,r,"roi :",mean_val)
+            cv2.circle(output, (x, y), r, (0, 255, 0) if mean_val >= 30 else (255, 0, 0), 3)
+            circle_centers.append((x, y, r))  
+            circle_colors.append("blanc" if mean_val >= 50 else "noir")  
             circle_count += 1  
-
             cv2.circle(output, (x, y), 3, (255, 0, 0), -1)
 
-        print("Coordonnées des centres des cercles détectés:", centers_list)
+        print("Coordonnées des centres des cercles détectés:", circle_centers)
         print("Nombre total de cercles détectés:", circle_count)
 
         while True:
-            circles_merged = False  # Indique si des cercles ont été fusionnés lors de cette itération
-            for i in range(len(centers_list), 0, -1):
-                for j in range(len(centers_list) - 1, i, -1):  
-                    x1, y1, r1 = centers_list[i]
-                    x2, y2, r2 = centers_list[j]
+            circles_merged = False  
+            for i in range(len(circle_centers) - 1, 0, -1):
+                for j in range(i - 1, -1, -1):  
+                    x1, y1, r1 = circle_centers[i]
+                    x2, y2, r2 = circle_centers[j]
                     dist_centers = np.sqrt((np.int64(x1) - np.int64(x2))**2 + (np.int64(y1) - np.int64(y2))**2)  
                     if dist_centers < 50:
-                        print("distance entre les cercles : ", dist_centers, "cercles 1 :", centers_list[i], "cercles 1 :", centers_list[j])
+                        print("distance entre les cercles : ", dist_centers, "cercles 1 :", circle_centers[i], "cercles 1 :", circle_centers[j])
                         print(f"Les centres des cercles {i+1} et {j+1} sont proches, fusion en cours...")
-                        # Calculer le nouveau centre comme le milieu des deux cercles
                         new_x = int((x1 + x2) / 2)
                         new_y = int((y1 + y2) / 2)
-                        # Calculer le nouveau rayon comme la moyenne des rayons des deux cercles
                         new_r = int((r1 + r2) / 2)
-                        # Ajouter le nouveau cercle fusionné à la liste
-                        centers_list.append((new_x, new_y, new_r))
-                        # Marquer le nouveau cercle sur l'image
-                        cv2.circle(output, (new_x, new_y), new_r, (255, 0, 0), 3)
-                        # Dessiner un point de couleur au centre du nouveau cercle fusionné
-                        cv2.circle(output, (new_x, new_y), 3, (255, 0, 0), -1)
-                        # Afficher les nouvelles coordonnées
+                        circle_centers.append((new_x, new_y, new_r))
+                        roi = binary[new_y-new_r:new_y+new_r, new_x-new_r:new_x+new_r]
+                        mean_val = np.mean(roi)
+                        print(new_x,new_y,new_r,"roi :",mean_val)
+                        circle_colors.append("blanc" if mean_val >= 50 else "noir")
                         print(f"Nouveau cercle à ({new_x}, {new_y}) avec un rayon de {new_r}")
-                        # Supprimer les cercles fusionnés de la liste
-                        del centers_list[j]
-                        del centers_list[i]
-                        # Mettre à jour le compteur de cercles
+                        del circle_centers[i]
+                        del circle_centers[j]
+                        del circle_colors[i]
+                        del circle_colors[j]
                         circle_count -= 1
                         circles_merged = True
                         break  
@@ -65,44 +61,31 @@ def detect_circles(image_path):
                     break  
 
             if not circles_merged:
-                break  # Sortir de la boucle externe si aucune fusion n'a eu lieu
+                break  
 
         print("Nombre de cercles après fusion:", circle_count)
-        merged = centers_list.copy()
+        merged = circle_centers.copy()
+        merged_colors = circle_colors.copy()
         print("nouveaux cercles", merged)
 
-        # Partie détection de couleur
-        circle_brightness = []  # Liste pour stocker la luminosité des cercles
-        for (x, y, r) in merged:
-
-            roi = binary[y-r:y+r, x-r:x+r]
-            mean_val = np.mean(roi)
-
-            if mean_val > 80:
-                circle_brightness.append("blanc")
-            else:
-                circle_brightness.append("noir")
-
-        print("Luminosité des cercles fusionnés:", circle_brightness)
-
-        # Créez une copie de l'image d'origine
+        # Affichage des cercles avec les couleurs appropriées
         merged_image = img.copy()
         output = cv2.resize(output, (800, 800))
 
-        # Dessinez les cercles fusionnés sur l'image fusionnée avec les coordonnées du centre
-        for (x, y, r) in merged:
-            color = (0, 255, 0) if circle_brightness[merged.index((x, y, r))] == "blanc" else (255, 0, 0)
-            cv2.circle(merged_image, (x, y), r, color, 3)
-            # Dessiner un point de couleur au centre du cercle fusionné
-            cv2.circle(merged_image, (x, y), 3, color, -1)
-            # Ajouter le texte avec les coordonnées du centre
+        for (x, y, r), color in zip(merged, merged_colors):
+            if color == "blanc":
+                cv2.circle(merged_image, (x, y), r, (0, 255, 0), 3)  # vert pour les cercles blancs
+            else:
+                cv2.circle(merged_image, (x, y), r, (255, 0, 0), 3)  # bleu pour les cercles noirs
+
+            cv2.circle(merged_image, (x, y), 3, (255, 255, 255), -1)
             cv2.putText(merged_image, f'({x}, {y})', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
-        # Affichez les deux images côte à côte
+        # Affichage de l'image fusionnée
         merged_image = cv2.resize(merged_image, (800, 800))
         cv2.imshow('output_and_merged', np.hstack([output, merged_image]))
         cv2.waitKey(0)
-
+    print("Liste des couleurs des cercles fusionnés:", merged_colors)
     return output, circle_count, merged
 
 img = "reflet.jpg"
--- test_dect.py
+++ test_dect.py
@@ -6,80 +6,87 @@ def detect_circles(image_path):
     output = img.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 5)
+    _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=30, maxRadius=60)
 
-    circle_count = 0  # Compteur de cercles détectés
+    circle_count = 0  
+    circle_centers = []  
+    circle_colors = []  
 
     if circles is not None:
         detected_circles = np.uint16(np.around(circles))
-        centers_list = []  # Liste pour stocker les coordonnées des centres des cercles détectés
+
         for (x, y, r) in detected_circles[0, :]:
-            cv2.circle(output, (x, y), r, (0, 255, 0), 3)
-            # Ajoute les coordonnées du centre et du rayon à la liste
-            centers_list.append((x, y, r))  
-            circle_count += 1  # Incrémente le compteur de cercles
-            # Dessiner un point de couleur au centre du cercle détecté
+            r-=10
+            roi = binary[y-r:y+r, x-r:x+r]
+            mean_val = np.mean(roi)
+            print (x,y,r,"roi :",mean_val)
+            cv2.circle(output, (x, y), r, (0, 255, 0) if mean_val >= 30 else (255, 0, 0), 3)
+            circle_centers.append((x, y, r))  
+            circle_colors.append("blanc" if mean_val >= 50 else "noir")  
+            circle_count += 1  
             cv2.circle(output, (x, y), 3, (255, 0, 0), -1)
 
-        print("Coordonnées des centres des cercles détectés:", centers_list)
+        print("Coordonnées des centres des cercles détectés:", circle_centers)
         print("Nombre total de cercles détectés:", circle_count)
-        # Boucle de fusion des cercles jusqu'à ce qu'aucune paire ne soit suffisamment proche
+
         while True:
-            circles_merged = False  # Indique si des cercles ont été fusionnés lors de cette itération
-            for i in range(len(centers_list),0,-1):
-                for j in range(len(centers_list) - 1, i, -1):  # Utiliser une boucle en sens inverse
-                    x1, y1, r1 = centers_list[i]
-                    x2, y2, r2 = centers_list[j]
+            circles_merged = False  
+            for i in range(len(circle_centers) - 1, 0, -1):
+                for j in range(i - 1, -1, -1):  
+                    x1, y1, r1 = circle_centers[i]
+                    x2, y2, r2 = circle_centers[j]
                     dist_centers = np.sqrt((np.int64(x1) - np.int64(x2))**2 + (np.int64(y1) - np.int64(y2))**2)  
                     if dist_centers < 50:
-                        print("distance entre les cercles : ", dist_centers,"cercles 1 :",centers_list[i],"cercles 1 :", centers_list[j])
+                        print("distance entre les cercles : ", dist_centers, "cercles 1 :", circle_centers[i], "cercles 1 :", circle_centers[j])
                         print(f"Les centres des cercles {i+1} et {j+1} sont proches, fusion en cours...")
-                        # Calculer le nouveau centre comme le milieu des deux cercles
                         new_x = int((x1 + x2) / 2)
                         new_y = int((y1 + y2) / 2)
-                        # Calculer le nouveau rayon comme la moyenne des rayons des deux cercles
                         new_r = int((r1 + r2) / 2)
-                        # Ajouter le nouveau cercle fusionné à la liste
-                        centers_list.append((new_x, new_y, new_r))
-                        # Marquer le nouveau cercle sur l'image
-                        cv2.circle(output, (new_x, new_y), new_r, (255, 0, 0), 3)
-                        # Dessiner un point de couleur au centre du nouveau cercle fusionné
-                        cv2.circle(output, (new_x, new_y), 3, (255, 0, 0), -1)
-                        # Afficher les nouvelles coordonnées
+                        circle_centers.append((new_x, new_y, new_r))
+                        roi = binary[new_y-new_r:new_y+new_r, new_x-new_r:new_x+new_r]
+                        mean_val = np.mean(roi)
+                        print(new_x,new_y,new_r,"roi :",mean_val)
+                        circle_colors.append("blanc" if mean_val >= 50 else "noir")
                         print(f"Nouveau cercle à ({new_x}, {new_y}) avec un rayon de {new_r}")
-                        # Supprimer les cercles fusionnés de la liste
-                        del centers_list[j]
-                        del centers_list[i]
-                        # Mettre à jour le compteur de cercles
+                        del circle_centers[i]
+                        del circle_centers[j]
+                        del circle_colors[i]
+                        del circle_colors[j]
                         circle_count -= 1
                         circles_merged = True
-                        break  # Sortir de la boucle interne après la fusion
+                        break  
                 if circles_merged:
-                    break  # Sortir de la boucle externe si des cercles ont été fusionnés
+                    break  
 
             if not circles_merged:
-                break  # Sortir de la boucle externe si aucune fusion n'a eu lieu
+                break  
 
         print("Nombre de cercles après fusion:", circle_count)
-        merged = centers_list.copy()
-        print("nouveaux cercles",merged)
-        
-        # Créez une copie de l'image d'origine
+        merged = circle_centers.copy()
+        merged_colors = circle_colors.copy()
+        print("nouveaux cercles", merged)
+
+        # Affichage des cercles avec les couleurs appropriées
         merged_image = img.copy()
-
-        # Dessinez les cercles fusionnés sur l'image fusionnée
-        for (x, y, r) in merged:
-            cv2.circle(merged_image, (x, y), r, (0, 0, 255), 3)
-            # Dessiner un point de couleur au centre du cercle fusionné
-            cv2.circle(merged_image, (x, y), 3, (0, 0, 255), -1)
-
-        # Affichez les deux images côte à côte
         output = cv2.resize(output, (800, 800))
+
+        for (x, y, r), color in zip(merged, merged_colors):
+            if color == "blanc":
+                cv2.circle(merged_image, (x, y), r, (0, 255, 0), 3)  # vert pour les cercles blancs
+            else:
+                cv2.circle(merged_image, (x, y), r, (255, 0, 0), 3)  # bleu pour les cercles noirs
+
+            cv2.circle(merged_image, (x, y), 3, (255, 255, 255), -1)
+            cv2.putText(merged_image, f'({x}, {y})', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Affichage de l'image fusionnée
         merged_image = cv2.resize(merged_image, (800, 800))
         cv2.imshow('output_and_merged', np.hstack([output, merged_image]))
         cv2.waitKey(0)
-
+    print("Liste des couleurs des cercles fusionnés:", merged_colors)
     return output, circle_count, merged
 
-img ="4.jpg"
+img = "reflet.jpg"
 detect_circles(img)
